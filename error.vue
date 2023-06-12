98b38723fc3aaf0c7f9816780e4ca22f97333b32
Noxerio <woinyc@seznam.cz>
2023-06-12 19:40:00 +0200

    error

---
+++ error.vue
@@ -0,0 +1,15 @@
+<script>
+    // This will work in both `<script setup>` and `<script>`
+    definePageMeta({
+    layout: "sub",
+    });
+</script>
+
+<template>
+   <NuxtLayout>
+    <div class="h-screen flex justify-center items-center">
+      <h1 class="text-6xl font-bold text center">Stránka nenalezena!</h1>
+    </div>
+      
+    </NuxtLayout>
+</template>
