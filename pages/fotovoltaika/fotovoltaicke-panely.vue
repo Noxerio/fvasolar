e26e17720c8265dd3fd8a7d5e58bcdd2d4a93784
Noxerio <woinyc@seznam.cz>
2023-07-09 22:53:59 +0200

    Fotovoltaicke panely

--- pages/fotovoltaika/fotovoltaicke-panely.vue
+++ pages/fotovoltaika/fotovoltaicke-panely.vue
@@ -1,4 +1,4 @@
-<script>
+<script setup>
     definePageMeta({
     layout: "sub",
     });
@@ -6,31 +6,38 @@
 
 <template>
     <AssetsSubHeader :title="'Fotovoltaické panely'"/>
-         <div class="container mx-auto p-8">
-    <h1 class="text-3xl font-bold mb-4">Existuje několik druhů fotovoltaických panelů:</h1>
-    
-    <ol class="list-decimal pl-6">
-      <li class="mb-4">
-        <h2 class="text-xl font-bold">Monokrystalické solární panely:</h2>
-        <p class="mb-2">Tyto panely jsou vyrobeny z jednoho kusu krystalu křemíku a mají nejvyšší účinnost využití slunečního záření. Jsou však také nejdražší a mají vyšší náklady na výrobu.</p>
-      </li>
-      
-      <li class="mb-4">
-        <h2 class="text-xl font-bold">Polykrystalické solární panely:</h2>
-        <p class="mb-2">Tyto panely jsou vyrobeny z křemíkových krystalů, které jsou seskupeny do bloků. Mají nižší účinnost než monokrystalické panely, ale jsou levnější a mají nižší náklady na výrobu.</p>
-      </li>
-      
-      <li class="mb-4">
-        <h2 class="text-xl font-bold">Tenké vrstvy solárních panelů:</h2>
-        <p class="mb-2">Tyto panely jsou vyrobeny pomocí nanotechnologie, která nanáší vrstvy fotovoltaických materiálů na substrát. Jsou tenčí a flexibilnější než jiné druhy panelů, ale mají nižší účinnost a kratší životnost.</p>
-      </li>
-      
-      <li class="mb-4">
-        <h2 class="text-xl font-bold">Koncentrované solární panely:</h2>
-        <p class="mb-2">Tyto panely využívají čočky nebo zrcadla k soustředění slunečního záření na malou plochu panelu. Mají vyšší účinnost než jiné druhy panelů, ale jsou nákladnější a vyžadují speciální zařízení pro instalaci.</p>
-      </li>
-    </ol>
-    
-    <p class="mt-4">Každý druh panelu má své výhody a nevýhody a vhodnost pro konkrétní aplikaci závisí na různých faktorech, jako jsou náklady, výkon, prostorové omezení a podmínky prostředí.</p>
+        <div class="container mx-auto p-8">
+          <div class="grid md:grid-cols-2 gap-8 my-5">
+            <div class="h-full flex items-center">
+              <img src="/img/solar_single.png" class=" h-auto mx-auto hidden md:flex items-center p-32">
+            </div>
+            <div class="grid md:grid-cols-2 gap-8 my-5">
+              <NuxtLink to="/fotovoltaika/monokrystalicke-fotovoltaicke-panely">
+                <div class="rounded border-2 border-slate-200 p-10 hover:text-white hover:bg-slate-800 transition-all cursor-pointer h-full">
+                  <h2 class="text-xl font-bold mb-4">Monokrystalické solární panely</h2>
+                  <p class="mb-2 opacity-85 leading-7">Tyto panely jsou vyrobeny z jednoho kusu krystalu křemíku a mají nejvyšší účinnost využití slunečního záření. Jsou však také nejdražší a mají vyšší náklady na výrobu.</p>
+                </div>
+              </NuxtLink>
+              <NuxtLink to="/fotovoltaika/polykrystalicke-solarni-panely">
+              <div class="rounded border-2 border-slate-200 p-10 hover:text-white hover:bg-slate-800 transition-all cursor-pointer h-full">
+                <h2 class="text-xl font-bold mb-4">Polykrystalické solární panely</h2>
+                <p class="mb-2 opacity-85 leading-7">Tyto panely jsou vyrobeny z křemíkových krystalů, které jsou seskupeny do bloků. Mají nižší účinnost než monokrystalické panely, ale jsou levnější a mají nižší náklady na výrobu.</p>
+              </div>
+             </NuxtLink>
+              <NuxtLink to="/fotovoltaika/tenkovrstve-fotovoltaicke-panely">
+              <div class="rounded border-2 border-slate-200 p-10 hover:text-white hover:bg-slate-800 transition-all cursor-pointer h-full">
+                <h2 class="text-xl font-bold mb-4">Tenké vrstvy solárních panelů</h2>
+                <p class="mb-2 opacity-85 leading-7">Tyto panely jsou vyrobeny pomocí nanotechnologie, která nanáší vrstvy fotovoltaických materiálů na substrát. Jsou tenčí a flexibilnější než jiné druhy panelů, ale mají nižší účinnost a kratší životnost.</p>
+              </div>
+              </NuxtLink>
+              <NuxtLink to="/fotovoltaika/koncentrovane-solarni-panely">
+              <div class="rounded border-2 border-slate-200 p-10 hover:text-white hover:bg-slate-800 transition-all cursor-pointer h-full">
+                <h2 class="text-xl font-bold mb-4">Koncentrované solární panely</h2>
+                <p class="mb-2 opacity-85 leading-7">Tyto panely využívají čočky nebo zrcadla k soustředění slunečního záření na malou plochu panelu. Mají vyšší účinnost než jiné druhy panelů, ale jsou nákladnější a vyžadují speciální zařízení pro instalaci.</p>
+              </div>
+            </NuxtLink>
+            </div>
+          </div>     
+    <p class="mt-4 text-center font-light">Každý druh panelu má své výhody a nevýhody a vhodnost pro konkrétní aplikaci závisí na různých faktorech, jako jsou náklady, výkon, prostorové omezení a podmínky prostředí.</p>
   </div>
 </template>
---
+++ pages/zaruka-a-servis.vue
@@ -0,0 +1,34 @@
+<script setup>
+ definePageMeta({
+    layout: "sub",
+    });
+</script>
+<template>
+    <div>
+        <AssetsSubHeader :title="'Záruky a servis'"/>
+        <div class="container mx-auto px-5 grid md:grid-cols-2 my-8">
+            <div class="mb-8">
+                <h3 class="mb-4 font-semibold text-2xl opacity-90">Na zařízení se vztahují tyto záruky</h3>
+                <ul class="list-disc pl-5 opacity-85 leading-8 text-lg">
+                    <li>25 let na 80% výkon fotovoltaického panelu</li>
+                    <li>7 let na baterie (10 let po registraci u výrobce)</li>
+                    <li>5 let na střídače</li>
+                    <li>15 let na mechanické části fotovoltaických panelů</li>
+                    <li>5 let odpovědnosti za případné vady práce</li>
+                    <li>2 roky na běžné komponenty </li>
+                </ul>
+            </div>
+            <div class="mb-8">
+                <h3 class="mb-4 font-semibold  text-2xl opacity-90">Záruční i pozáruční servis</h3>
+                <ul class="list-disc pl-5 opacity-85 leading-8 text-lg">
+                    <li>Poskytujeme záruční a pozáruční servis </li>
+                    <li>Automatizace detekce chyb díky chytrému monitoringu </li>
+                    <li>Vzdálené aktualizace nastavených komponentů </li>
+                    <li>Dodáváme kvalitní komponenty výrobců dlouho působících na trhu</li>
+                    <li>Neváhejte poptat servis</li>
+                </ul>
+            </div>
+        </div>
+        <SectionsGuaranty/>
+    </div>
+</template>
