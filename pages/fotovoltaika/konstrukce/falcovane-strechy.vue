015297947b682ccd141a0e1a1150543f48072dd5
Noxerio <woinyc@seznam.cz>
2023-07-18 01:25:24 +0200

    Konstrukce / First

---
+++ pages/fotovoltaika/konstrukce/falcovane-strechy.vue
@@ -0,0 +1,24 @@
+<script setup>
+    definePageMeta({
+    layout: "sub",
+    });
+</script>
+<template>
+    <section>
+        <AssetsSubHeader :title="'Falcované střechy'"/>
+        <div class="container mx-auto px-5 my-10">
+            <div class="grid md:grid-cols-2 gap-8 mb-8">
+                <div></div>
+                <div>
+                    <ul class="leading-8 list-disc pl-5">
+                        <li>Různé druhy příchytek nám umožní uchycení na jakýkoliv falcovaný plech</li>
+                        <li>Kotvení přímo na falc bez zásahů do střechy</li>
+                        <li>Snadná montáž a demontáž</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <SectionsFotovoltaikaTypes/>
+    <SectionsFotovoltaikaProces/>
+</template>
