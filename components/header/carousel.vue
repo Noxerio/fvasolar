67cc9d8b4e4533cf6bed389d7d5867fba4bcaf7a
Noxerio <woinyc@seznam.cz>
2023-07-25 18:53:56 +0200

    upp

--- components/header/carousel.vue
+++ components/header/carousel.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="w-full bg-[url('/img/bg-top.png')] bg-cover bg-center bg-fixed">
+    <div class="w-full bg-slate-50 bg-cover bg-center bg-fixed border-b-4 border-amber-400">
         <div class="w-full container mx-auto grid md:grid-cols-2 gap-16 items-center py-16 md:py-32 px-5">
   
             <div v-motion-slide-left class="flex flex-col items-start justify-center gap-8 ">
-                <h1 class="text-white font-bold text-4xl md:text-6xl uppercase">
-                    Fotovoltaická elektrárna na klíč</h1>
-                <p class="text-white md:text-lg ">Hledáte partnerskou firmu pro Vaši fotovoltaickou elektrárnu? To jste tady správně! Vyplňte poptávkový formulář a do 2 dnů Vás budeme kontaktovat. Sjednejte si schůzku s našim obchodním zástupcem, zjistěte, zda splňujete všechny požadavky pro výstavbu fotovoltaické elektrárny a zároveň získání dotace. Se Vším Vám pomůžeme a vyřešíme za Vás. Těšíme se na budoucí spolupráci.</p>
+                <h1 class=" font-extrabold text-4xl md:text-6xl uppercase" style="line-height: 75px;">
+                    <span class="text-amber-400 text-stroke-3 drop-shadow-xs">
+                        Fotovoltaická 
+                    </span>elektrárna na klíč</h1>
+                <p class=" md:text-lg ">Hledáte partnerskou firmu pro Vaši fotovoltaickou elektrárnu? To jste tady správně! Vyplňte poptávkový formulář a do 2 dnů Vás budeme kontaktovat. Sjednejte si schůzku s našim obchodním zástupcem, zjistěte, zda splňujete všechny požadavky pro výstavbu fotovoltaické elektrárny a zároveň získání dotace. Se Vším Vám pomůžeme a vyřešíme za Vás. Těšíme se na budoucí spolupráci.</p>
                 <button class="bg-amber-400 text-slate-800 px-8 py-3 rounded-lg font-bold"><Icon name="material-symbols:arrow-circle-down-outline" size="1.5rem"/> Pojdmě se podívat dál</button>
             </div>
             <div  class="justify-center items-ce nter hidden md:flex ">
