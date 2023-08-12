129345d8968f37efb70453a45528a78d1e889283
Noxerio <woinyc@seznam.cz>
2023-08-12 11:31:15 +0200

    Update menu

--- components/navbar/main.vue
+++ components/navbar/main.vue
@@ -89,23 +89,35 @@
           <li>
               <button id="dropdownNavbarLink" data-dropdown-toggle="dropKlient" class="flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Klientská zóna<svg class="w-5 h-5 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></button>
               <!-- Dropdown menu -->
-              <div id="dropKlient" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600">
-                  <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
+              <div id="dropKlient" class="z-50 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600 w-full">
+                <div class="container mx-auto">
+                  <ul class="py-2 text-sm text-gray-700 dark:text-gray-400 grid md:grid-cols-4 md:gap-8 md:py-8 px-8 md:px-0" aria-labelledby="dropdownLargeButton">
                     <li>
-                      <NuxtLink to="/reference" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                      <NuxtLink to="/reference" class="rounded-xl block px-4 py-2 md:text-lg md:font-bold hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white md:flex md:items-center">
+                        <img class="md:block hidden" src="/img/nav/nav_9.png">
                         Reference
                       </NuxtLink>
-                      <NuxtLink to="https://www.solaxcloud.com/#/login" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                    </li>
+                    <li>
+                      <NuxtLink to="https://www.solaxcloud.com/#/login" class="rounded-xl block px-4 py-2 md:text-lg md:font-bold hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white md:flex md:items-center">
+                        <img class="md:block hidden" src="/img/nav/nav_12.png">
                         Monitoring
                       </NuxtLink>
-                      <NuxtLink to="/soubory" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                        Dokumenty
+                    </li>
+                    <li>
+                      <NuxtLink to="/nova-zelena-usporam" class=" rounded-xl block px-4 py-2 md:text-lg md:font-bold hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white md:flex md:items-center">
+                        <img class="md:block hidden" src="/img/nav/nav_10.png">
+                        Aplikace 
                       </NuxtLink>
-                      <NuxtLink to="/" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                        Nezávazná poptávka
+                    </li>
+                    <li>
+                      <NuxtLink to="/soubory" class=" rounded-xl block px-4 py-2 md:text-lg md:font-bold hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white md:flex md:items-center">
+                        <img class="md:block hidden" src="/img/nav/nav_11.png">
+                        Dokumenty
                       </NuxtLink>
                     </li>
                   </ul>
+                </div>  
               </div>
           </li>
           <li>
