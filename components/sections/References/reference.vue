b497108766b4f4f8b5827f8b152568004fa700b0
Noxerio <woinyc@seznam.cz>
2023-09-04 14:13:30 +0200

    image update

--- components/sections/References/reference.vue
+++ components/sections/References/reference.vue
@@ -5,7 +5,7 @@
 <template>
     <div class="h-76 rounded-xl hover: hover:scale-110 transition-all duration-700 relative flex justify-center">
         <NuxtLink :to="props.path">
-            <NuxtImg class=" w-full h-full rounded-xl relative shadow-2xl transition-all duration-700 grayscale hover:grayscale-0 " :src="props.image" placeholder />
+            <NuxtImg class=" w-full h-full rounded-xl relative shadow-2xl transition-all duration-700 grayscale hover:grayscale-0 " :src="props.image"/>
         </NuxtLink>
     </div>
 </template>
