4d7953649d4d7556cadd8a15e114a5692ac7901c
Noxerio <woinyc@seznam.cz>
2025-04-09 20:28:51 +0200

    Kontakt update

--- components/sections/contact.vue
+++ components/sections/contact.vue
@@ -4,7 +4,7 @@
 
             <div class="grid md:grid-cols-2 gap-8">
                 <ul class="rounded-2xl p-10">
-                    <li class="flex gap-2 text-xl font-bold pb-5 uppercase">FVA Solar s.r.o.</li>
+                    <li class="flex gap-2 text-xl font-bold pb-5 uppercase">FVA Solar, s.r.o.</li>
                     <li class="flex gap-2"><span class="font-bold">IČO:</span>14267560</li>
                     <li class="flex gap-2"><span class="font-bold">DIČ:</span>CZ14267560</li>
                     <li class="flex gap-2"><span class="font-bold">Steuernummer:</span>214/173/23187</li>
@@ -14,7 +14,7 @@
                     <li class="flex gap-2"><span class="font-bold">Bankovní spojení EUR:</span>2102125778/2010</li>
                 </ul>
                 <ul class=" rounded-2xl p-10">
-                    <li class="flex gap-2 text-xl font-bold pb-5 uppercase">FVA Montáže s.r.o.</li>
+                    <li class="flex gap-2 text-xl font-bold pb-5 uppercase">FVA Montáže, s.r.o.</li>
                     <li class="flex gap-2"><span class="font-bold">IČO:</span>17536421</li>
                     <li class="flex gap-2"><span class="font-bold">DIČ:</span>CZ17536421</li>
                     <li class="flex gap-2"><span class="font-bold">Sídlo:</span>Paskovská 636/275, Hrabová, 720 00 Ostrava</li>
